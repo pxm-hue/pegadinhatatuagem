97db9dbfba2843f62fadff28b8d0413858df55ed
pxm-hue <pedrohpxm@gmail.com>
2026-07-27 22:28:26 -0300

    Update status-pagamento.js

--- api/status-pagamento.js
+++ api/status-pagamento.js
@@ -2,17 +2,25 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
   const { id } = req.query;
-  const ASAAS_API_KEY = 'SUA_CHAVE_ASAAS_AQUI'; 
+
+  if (!id) {
+    return res.status(400).json({ error: 'ID do pagamento não fornecido' });
+  }
+
+  const ASAAS_API_KEY = '$aact_prod_000MzkwODA2MWY2OGM3MWRlMDU2NWM3MzJlNzZmNGZhZGY6OjJhMzVlNzJjLTE2YTMtNGM1My05ZjE3LWQ5YzVkMTMzZDRjODo6JGFhY2hfYmJmY2Q0MmItNjY1MS00NjcwLTg2ZDktOTZlOGJkNTMzZTdi';
   const ASAAS_URL = 'https://www.asaas.com/api/v3';
 
   try {
-    const status = await axios.get(`${ASAAS_URL}/payments/${id}`, {
+    const response = await axios.get(`${ASAAS_URL}/payments/${id}`, {
       headers: { 'access_token': ASAAS_API_KEY }
     });
 
-    const estaPago = status.data.status === 'RECEIVED' || status.data.status === 'CONFIRMED';
+    const status = response.data.status;
+    const estaPago = status === 'RECEIVED' || status === 'CONFIRMED';
+
     return res.status(200).json({ pago: estaPago });
+
   } catch (error) {
-    return res.status(500).json({ error: 'Erro ao verificar pagamento' });
+    return res.status(500).json({ pago: false, error: 'Erro ao consultar pagamento' });
   }
 };
